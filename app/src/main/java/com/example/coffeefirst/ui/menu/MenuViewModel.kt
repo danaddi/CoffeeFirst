d5369c2d9db97e348f0f357ac916f94340395da9
danaddi <danadi1010@gmail.com>
2025-06-18 19:08:57 +0300

    Увеличено меню для демонстрации работы RecyclerView

--- app/src/main/java/com/example/coffeefirst/ui/menu/MenuViewModel.kt
+++ app/src/main/java/com/example/coffeefirst/ui/menu/MenuViewModel.kt
@@ -22,6 +22,11 @@ class MenuViewModel @Inject constructor(
     val menuItems = listOf(
         MenuItem("coffee1", "Капучино", R.drawable.latte, "Кофе", 150.0f),
         MenuItem("coffee2", "Латте", R.drawable.latte, "Кофе", 200.0f),
+        MenuItem("coffee3", "Раф", R.drawable.latte, "Кофе", 184.0f),
+        MenuItem("coffee4", "Эспрессо", R.drawable.latte, "Кофе", 130.0f),
+        MenuItem("coffee5", "Макиато", R.drawable.latte, "Кофе", 150.0f),
+        MenuItem("coffee6", "Фраппе", R.drawable.latte, "Кофе", 250.0f),
+        MenuItem("coffee7", "Американо", R.drawable.latte, "Кофе", 200.0f),
         MenuItem("tea1", "Зеленый чай", R.drawable.water, "Чай", 100.0f),
         MenuItem("food1", "Сэндвич", R.drawable.hamburger, "Еда", 120.0f),
         MenuItem("dessert1", "Чизкейк", R.drawable.cheesecake, "Десерты", 180.0f),
